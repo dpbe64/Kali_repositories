e6b3fd169fcb9256c6e43f25dbe8306255b07727
dpbe64 <72489999+dpbe64@users.noreply.github.com>
2020-10-12 13:46:27 +0200

    Update repositories_kali.sh

--- repositories_kali.sh
+++ repositories_kali.sh
@@ -3,10 +3,11 @@
 #you need to execute this program with root user
 #created by dpbe64
 
-	echo "[*] Adding repositories...\n"
+	echo "[*] You need to execute this program with user root"
+	echo "[*] Adding repositories..."
 	echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list
-	echo "[*] Installing wget...\n"
+	echo "[*] Installing wget..."
 	sudo apt install wget -y
-	echo "[*] wget installed succesfully\n"
+	echo "[*] wget installed succesfully"
 	wget http://archive.kali.org/archive-key.asc | apt-key add archive-key.asc
 	echo "[*] Now yo have a repositories of kali linux enjoy!"
